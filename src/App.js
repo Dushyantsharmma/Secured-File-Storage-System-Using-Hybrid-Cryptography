d6e54cf839fe3302226582633bc50ea010d9a419
Dushyant Sharma <90922202+Dushyantsharmma@users.noreply.github.com>
2023-11-22 08:46:09 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-
 import "./App.css";
 import { useState, useEffect } from "react";
 import {
@@ -6,21 +5,21 @@ import {
   uploadBytes,
   getDownloadURL,
   listAll,
-  list,
 } from "firebase/storage";
 import { storage } from "./firebase";
-import { v4 } from "uuid";
+import { v4 as uuidv4 } from "uuid";
 
 function App() {
   const [imageUpload, setImageUpload] = useState(null);
   const [imageUrls, setImageUrls] = useState([]);
 
-  
-
   const imagesListRef = ref(storage, "images/");
+
   const uploadFile = () => {
     if (imageUpload == null) return;
-    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    
+    const imageRef = ref(storage, `images/${uuidv4()}_${imageUpload.name}`);
+    
     uploadBytes(imageRef, imageUpload).then((snapshot) => {
       getDownloadURL(snapshot.ref).then((url) => {
         setImageUrls((prev) => [...prev, url]);
@@ -29,36 +28,32 @@ function App() {
   };
 
   useEffect(() => {
-    listAll(imagesListRef).then((response) => {
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          setImageUrls((prev) => [...prev, url]);
-        });
+    listAll(imagesListRef)
+      .then((response) => {
+        const promises = response.items.map((item) => getDownloadURL(item));
+        return Promise.all(promises);
+      })
+      .then((urls) => {
+        setImageUrls(urls);
+      })
+      .catch((error) => {
+        console.error("Error fetching images:", error);
       });
-    });
   }, []);
 
   return (
-
     <div className="App">
       <input
         type="file"
-  
         onChange={(event) => {
-      
           setImageUpload(event.target.files[0]);
-
-
-        } }
-  
+        }}
       />
-       
-      <button onClick={uploadFile}> Upload Image/File</button>
+      <button onClick={uploadFile}>Upload Image/File</button>
       <h1>File Upload</h1>
-      {imageUrls.map((url) => {
-        return <img src={url} />;
-
-      })}
+      {imageUrls.map((url, index) => (
+        <img key={index} src={url} alt={`Image ${index}`} />
+      ))}
     </div>
   );
 }
--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,24 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import {getStorage} from 'firebase/storage'
+import { getStorage } from 'firebase/storage';
+
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDnE3NfZ2fS8pOqJDB72r7sgMpvtFzpidE",
-  authDomain: "uploadingfile-78291.firebaseapp.com",
-  projectId: "uploadingfile-78291",
-  storageBucket: "uploadingfile-78291.appspot.com",
-  messagingSenderId: "254732356822",
-  appId: "1:254732356822:web:7ce3e3fbaf7a3500ec0d77"
+  apiKey: "AIzaSyDrghW3I0VyvYwXuTtIgXcsvs8LMh_lhW0",
+  authDomain: "hola-amigo-b3bea.firebaseapp.com",
+  projectId: "hola-amigo-b3bea",
+  storageBucket: "hola-amigo-b3bea.appspot.com",
+  messagingSenderId: "40434274714",
+  appId: "1:40434274714:web:9743c2454613035e5eaa23"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const storage = getStorage(app)
+const storage = getStorage(app);
+
+export { storage };
